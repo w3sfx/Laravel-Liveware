a8ce6517807b8b7474b9d77bcd3e3390603dcab0
Wesley Victor <w3sleyv1ctor@gmail.com>
2022-03-04 16:14:25 -0300

    Inserção de Registros / Validações / Paginação

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,13 +1,25 @@
 <div>
     Show Tweets
 
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
-    <input type="text" name="message" id="message" wire:model="message">
+    <form method="post" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+        @error('content')
+            {{ $message }}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
 
     <hr>
 
     @foreach ($tweets as $tweet)
         {{ $tweet->user->name }} - {{ $tweet->content }} <br>
     @endforeach
+
+    <hr>
+
+    <div>
+        {{ $tweets->links() }}
+    </div>
 </div>
